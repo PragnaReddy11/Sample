10041faf4c96127891ce1411c901db03e67023c4
Machupalli Sree Pragna <88078068+PragnaReddy11@users.noreply.github.com>
2023-02-27 15:49:04 +0530

    Add files via upload

---
+++ src/assets/data/data.js
@@ -0,0 +1,254 @@
+import img01 from "../images/img-01.jpg";
+import img02 from "../images/img-02.jpg";
+import img03 from "../images/img-03.jpg";
+import img04 from "../images/img-04.jpg";
+import img05 from "../images/img-05.jpg";
+import img06 from "../images/img-06.jpg";
+import img07 from "../images/img-07.jpg";
+import img08 from "../images/img-08.jpg";
+import img09 from "../images/img-09.jpg";
+
+import ava01 from "../images/ava-01.png";
+import ava02 from "../images/ava-02.png";
+import ava03 from "../images/ava-03.png";
+import ava04 from "../images/ava-04.png";
+import ava05 from "../images/ava-05.png";
+import ava06 from "../images/ava-06.png";
+
+export const NFT__DATA = [
+  {
+    id: "01",
+    title: "Rohit Sharma",
+    desc: "India",
+    imgUrl: img01,
+    creator: "ICC",
+    creatorImg: ava01,
+    currentBid: 5🪙,
+  },
+
+  {
+    id: "02",
+    title: "Virat Kohli",
+    desc: "India",
+    imgUrl: img02,
+    creator: "ICC",
+    creatorImg: ava02,
+    currentBid: 5🪙,
+  },
+
+  {
+    id: "03",
+    title: "Smriti Mandhana",
+    desc: "India",
+    imgUrl: img03,
+    creator: "ICC",
+    creatorImg: ava03,
+    currentBid: 5🪙,
+  },
+
+  {
+    id: "04",
+    title: "Kane Williamson",
+    desc: "New Zeland",
+    imgUrl: img04,
+    creator: "ICC",
+    creatorImg: ava04,
+    currentBid: 5🪙,
+  },
+
+  {
+    id: "05",
+    title: "David Warner",
+    desc: "Australia",
+    imgUrl: img05,
+    creator: "ICC",
+    creatorImg: ava05,
+    currentBid: 5🪙,
+  },
+
+  {
+    id: "06",
+    title: "Meg Lanning",
+    desc: "Australia",
+    imgUrl: img06,
+    creator: "ICC",
+    creatorImg: ava06,
+    currentBid: 5🪙,
+  },
+
+  {
+    id: "07",
+    title: "Jofra Archer",
+    desc: "England",
+    imgUrl: img07,
+    creator: "ICC",
+    creatorImg: ava03,
+    currentBid: 3🪙,
+  },
+
+  {
+    id: "08",
+    title: "Shreyas Iyer",
+    desc: "India",
+    imgUrl: img08,
+    creator: "ICC",
+    creatorImg: ava04,
+    currentBid: 3🪙,
+  },
+
+  {
+    id: "09",
+    title: "Elyssy Perry",
+    desc: "Australia",
+    imgUrl: img09,
+    creator: "ICC",
+    creatorImg: ava05,
+    currentBid: 3🪙,
+  },
+];
+
+export const SELLER__DATA = [
+  {
+    id: "01",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+    sellerName: "Ryan Carder",
+    sellerImg: ava01,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "02",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+    sellerName: "Trista Francis",
+    sellerImg: ava02,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "03",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+    sellerName: "Ryan Carder",
+    sellerImg: ava03,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "04",
+
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+
+    sellerName: "Ryan Carder",
+    sellerImg: ava04,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "05",
+
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+
+    sellerName: "Trista Francis",
+    sellerImg: ava05,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "06",
+
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+
+    sellerName: "Trista Francis",
+    sellerImg: ava06,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "01",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+    sellerName: "Ryan Carder",
+    sellerImg: ava01,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "02",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+    sellerName: "Trista Francis",
+    sellerImg: ava02,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "03",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+    sellerName: "Ryan Carder",
+    sellerImg: ava03,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "04",
+
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+
+    sellerName: "Ryan Carder",
+    sellerImg: ava04,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "05",
+
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+
+    sellerName: "Trista Francis",
+    sellerImg: ava05,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+
+  {
+    id: "06",
+
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia",
+
+    sellerName: "Trista Francis",
+    sellerImg: ava06,
+    currentBid: 5.89,
+    fbUrl: "#",
+    instaUrl: "#",
+    twitUrl: "#",
+  },
+];
